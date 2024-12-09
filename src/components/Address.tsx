fa406f982f0c7fc3f22d3b9f8840499d288ad9d1
Garth <garth@athena.com.au>
2024-12-09 13:57:05 +1100

    Fix.

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -2,12 +2,11 @@ import useAddressAutocomplete from "../hooks/useAddressAutocomplete";
 // import useRates from "../hooks/useRates";
 
 export const Address = () => {
-  const { data = [] } = useAddressAutocomplete();
-  // console.log({ data });
+  const { data = [{ id: 0, name: "" }] } = useAddressAutocomplete();
   return (
     <div>
       <h2>Address</h2>
-      <p>{data?.length && data[0].name}</p>
+      <p>{data && data[0].name}</p>
     </div>
   );
 };
