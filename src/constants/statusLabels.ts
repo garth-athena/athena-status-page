212c9c9d4e07a43f1b1c6a3c765b30179461e502
Nishant Sharma <nishant.sharma@athena.com.au>
2024-12-09 17:47:10 +1100

    Added 403 and 304 Labels

--- src/constants/statusLabels.ts
+++ src/constants/statusLabels.ts
@@ -2,7 +2,9 @@ export const statusLabels = {
   0: "Pending",
   200: "Operational",
   204: "Operational",
+  304: "Operational",
   401: "Impacted",
   403: "Impacted",
+  404: "Impacted",
   500: "Impacted",
 };
